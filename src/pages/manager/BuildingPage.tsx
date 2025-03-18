9dc4d3fb6540baac70d4a098748dbc6da4bddef7
Yasser <aitelghariyasser@gmail.com>
2025-03-18 11:58:18 +0000

    add add and edit components  to buildingPage component

--- src/pages/manager/BuildingPage.tsx
+++ src/pages/manager/BuildingPage.tsx
@@ -1,6 +1,8 @@
 import Header from "../../components/layout/Header.tsx";
 import Footer from "../../components/layout/Footer.tsx";
 import {DashboardHeader} from "../../components/admin/DashboardHeader.tsx";
+import AddBuilding from "../../components/manager/building/AddBuilding.tsx";
+import Building from "../../components/manager/building/Building.tsx";
 
 
 const BuildingPage = () => {
@@ -9,10 +11,10 @@ const BuildingPage = () => {
             <Header/>
             <main>
                 <DashboardHeader page={"Building"}/>
-                < />
+                <AddBuilding/>
                 <div className="my-account-wrapper mt-no-text mb-no-text">
                     <div  className="container container-default-2 custom-area d-flex justify-content-center text-center">
-                        <B  />
+                        <Building  />
                     </div>
                 </div>
             </main>
